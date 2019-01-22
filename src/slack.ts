be9e457c8ad6558bd23c48818fb15573eebd384e
Eric So <me@eric.so>
2019-01-21 22:19:05 -0800

    send Authorization token

--- src/slack.ts
+++ src/slack.ts
@@ -1,8 +1,16 @@
 import axios from 'axios';
 
+const axiosClient = axios.create({
+  baseURL: 'https://slack.com/api/',
+  timeout: 1000,
+  headers: {
+    'Authorization': `Bearer ${process.env.SLACK_BOT_USER_OAUTH_ACCESS_TOKEN}`,
+  },
+});
+
 export const sendToSlack = (token: string, text: string, channel: string, rest?: any) => {
-  axios.post(
-    'https://slack.com/api/chat.postMessage',
+  axiosClient.post(
+    'chat.postMessage',
     {
       token,
       text,
